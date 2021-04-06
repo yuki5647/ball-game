ede7a1a7bb7da60ce7bdab595204cdb6380778e1
yukitosi <t.yuki.0527@gmail.com>
2021-04-06 19:30:16 +0900

    ball game
    ブロック出させてポイント稼がせる

---
+++ Assets/Script/cubeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour {
+    private GameObject Scoretext;
+    private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+        Scoretext = GameObject.Find("scoretext");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "balltag")
+        {
+            score += 100;
+            
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/playerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour {
+    [Range (0f, 30f)] public float speed = 10;
+    private void FixedUpdate()
+    {
+        float moveHorizontel = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontel, 0.0f, moveVertical);
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(movement * speed);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
