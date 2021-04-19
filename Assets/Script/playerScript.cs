7574c81aa09214a5bed0a08bde137592b6db6f19
yukitosi <t.yuki.0527@gmail.com>
2021-04-19 19:46:42 +0900

    ブロックゲーム　終了
    スコア更新、ゲーム終了のテキストできた

--- Assets/Script/playerScript.cs
+++ Assets/Script/playerScript.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class playerScript : MonoBehaviour {
+
+    public GameObject Scoretext;
+    private int score = 0;
+    public GameObject finish;
+    private float finishposY = -10.0f;
+
     [Range (0f, 30f)] public float speed = 10;
     private void FixedUpdate()
     {
+        Scoretext = GameObject.Find("scoretext");
+        finish = GameObject.Find("goaltext");
+
         float moveHorizontel = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -21,6 +32,18 @@ public class playerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (this.transform.position.y < this.finishposY)
+        {
+            finish.GetComponent<Text>().text = "GAME OVER";
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "cubetag") {
+            score += 10;
+            Debug.Log(score);
+            Scoretext.GetComponent<Text>().text = score + "pt";
+        }
+    }
 }
